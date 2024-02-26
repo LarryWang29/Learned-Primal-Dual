8437d05ce64233585d4409bf6337ab968398bd11
Larry Wang <dw661@cam.ac.uk>
2024-02-26 18:16:11 +0000

    Changed naming convension in PrimalDualNet and added more script for training

--- network_trainer.py
+++ network_trainer.py
@@ -4,35 +4,29 @@ from primal_dual_nets import PrimalDualNet
 from dataloader import DataSetHelper
 import utils
 from torch.utils.data import DataLoader
-
-# Specify the paths
-data_path = "./data/truth_training_pseudodataset/"
-target_path = "./data/observation_training_pseudodataset/"
-
-# Create a dataset object
-dataset = DataSetHelper(data_path, target_path, index=8)
-
-# Obtain the first image
-train_dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
-
-# Set a global seed for reproducibility
-torch.manual_seed(1029)
+import tqdm
 
 
 # Define a function that trains the network
 def train_network(input_dimension=362, n_detectors=513,
                   n_angles=1000, n_primal=5, n_dual=5, n_iterations=10,
-                  epochs=10, learning_rate=0.001, beta=0.99):
+                  epochs=25, learning_rate=0.001, beta=0.99):
     loss_function = nn.MSELoss()
 
-    # Obtain the ground truth
-    training_data = next(iter(train_dataloader))
-    ground_truth = training_data[0]
+    # Specify the paths
+    data_path = "./data/truth_training_pseudodataset/"
+    target_path = "./data/observation_training_pseudodataset/"
 
-    # Add noise to the ground truth
-    observation = utils.add_noise(ground_truth)
-    model = PrimalDualNet(observation,
-                          input_dimension=input_dimension,
+    # Create a dataset object
+    dataset = DataSetHelper(data_path, target_path, index=8)
+
+    # Obtain the first image
+    train_dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+    # Set a global seed for reproducibility
+    torch.manual_seed(1029)
+
+    model = PrimalDualNet(input_dimension=input_dimension,
                           n_detectors=n_detectors, n_angles=n_angles,
                           n_primal=n_primal, n_dual=n_dual,
                           n_iterations=n_iterations)
@@ -40,18 +34,81 @@ def train_network(input_dimension=362, n_detectors=513,
     optimizer = torch.optim.Adam(model.parameters(),
                                  lr=learning_rate, betas=(beta, 0.999))
 
+    # Set up a scheduler to set up cosine annealing
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
+
     for i in range(epochs):
+        # for batch in train_dataloader:
+            # Obtain the ground truth
+            # training_data = batch
+            # print(len(train_dataloader))
+        training_data = next(iter(train_dataloader))
+        ground_truth = training_data[0]
+
+        # # Print the model parameters
+        # for i in model.parameters():
+        #     print(i)
+
+        # Add noise to the ground truth
+        # observation = utils.add_noise(ground_truth)
+
+        observation = training_data[1]
+        print("Training...")
         # Zero the gradients
         optimizer.zero_grad()
 
         # Pass the observation through the network
-        output = model.forward()
+        output = model.forward(observation).squeeze(1)
+        # for i in model.parameters():
+        #     print(i)
+        # print(output)
         loss = loss_function(output, ground_truth)
 
         # Backward pass
         loss.backward()
-        optimizer.step()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0,
+                                    norm_type=2)
 
+        optimizer.step()
+        scheduler.step()
+
+        # Print out the loss, weights and biases in the model
         print(f"Epoch {i + 1}/{epochs}, Loss: {loss.item()}")
 
     return model
+
+
+model = train_network(n_primal=5, n_dual=5)
+
+# Predict the output
+model.eval()
+
+# Specify the paths
+data_path = "./data/truth_test_pseudodataset/"
+target_path = "./data/observation_test_pseudodataset/"
+
+# Create a dataset object
+dataset = DataSetHelper(data_path, target_path, index=0)
+
+# Obtain the first image
+train_dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+training_data = next(iter(train_dataloader))
+
+observation = training_data[1]
+observation = utils.add_noise(training_data[0])
+output = model.forward(observation).squeeze(1)
+
+# Visualiase the reconstruction with colour bar as well as the original image
+import matplotlib.pyplot as plt
+plt.figure()
+plt.subplot(1, 2, 1)
+plt.imshow(training_data[0].squeeze(0))
+plt.colorbar()
+plt.title("Ground Truth")
+plt.subplot(1, 2, 2)
+plt.imshow(output.detach().numpy().squeeze(0))
+plt.colorbar()
+plt.title("Reconstructed Image")
+plt.show()
+
+print("Done!")
--- primal_dual_nets.py
+++ primal_dual_nets.py
@@ -3,75 +3,11 @@ import torch
 import tomosipo as ts
 
 
-class PrimalNet(nn.Module):
-    """
-    Implementation of the primal network, using a 3-layer CNN.
-    """
-
-    def __init__(self, n_primal):
-        """
-        Initalisation function for the primal network. The architecture is
-        3-layer CNN with PReLU activations, the first two layers have 32
-        channels, and the last layer has n_primal channels.
-
-        Parameters
-        ----------
-        n_primal : int
-            The number of primal channels in "history"
-        """
-        super().__init__()
-
-        self.n_primal = n_primal
-
-        self.conv1 = nn.Conv2d(in_channels=n_primal + 2, out_channels=32,
-                               kernel_size=(3, 3), padding=1)
-        self.act1 = nn.PReLU()
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32,
-                               kernel_size=(3, 3), padding=1)
-        self.act2 = nn.PReLU()
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=n_primal,
-                               kernel_size=(3, 3), padding=1)
-
-    def forward(self, primal, f2, g):
-        """
-        Forward pass for the primal network. The inputs are all current
-        primals, second dual variable, and the sinogram. The output is the
-        updated primal.
-
-        Parameters
-        ----------
-        primal : torch.Tensor
-            Primal variable at the current iteration.
-        f2 : torch.Tensor
-            Forward projection of second dual variable at the current
-            iteration.
-        g : torch.Tensor
-            Observed sinogram.
-
-        Returns
-        -------
-        update : torch.Tensor
-            Update for primal variable.
-        """
-
-        # Concatenate primal, f2 and g
-        input = torch.cat((primal, f2, g), 0)
-
-        # Pass through the network
-        result = self.conv1(input)
-        result = self.act1(result)
-        result = self.conv2(result)
-        result = self.act2(result)
-        result = self.conv3(result)
-
-        # Add the result to primal and return the updated primal
-        return result
-
-
 class DualNet(nn.Module):
     """
     Implementation of the dual network, using a 3-layer CNN.
     """
+
     def __init__(self, n_dual):
         """
         Initalisation function for the dual network. The architecture is
@@ -83,27 +19,129 @@ class DualNet(nn.Module):
         n_dual : int
             The number of dual channels in "history"
         """
-        super().__init__()
+        super(DualNet, self).__init__()
 
         self.n_dual = n_dual
 
-        self.conv1 = nn.Conv2d(in_channels=n_dual + 1, out_channels=32,
+        self.conv1 = nn.Conv2d(in_channels=n_dual + 2, out_channels=32,
                                kernel_size=(3, 3), padding=1)
-        self.act1 = nn.PReLU()
+        self.act1 = nn.PReLU(num_parameters=32, init=0.0)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=32,
                                kernel_size=(3, 3), padding=1)
-        self.act2 = nn.PReLU()
+        self.act2 = nn.PReLU(num_parameters=32, init=0.0)
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=n_dual,
                                kernel_size=(3, 3), padding=1)
 
-    def forward(self, dual, fp_f1, adj_h1):
+        # Intialise the weights and biases
+        self._init_weights()
+
+    def _init_weights(self):
+        """
+        Initialises the weights of the network using the Xavier initialisation
+        method.
+        """
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # Initialise the weights using the Xavier initialisation method
+                nn.init.xavier_uniform_(m.weight)
+
+                # Initialise the biases to zero
+                nn.init.zeros_(m.bias)
+
+    def forward(self, dual, f2, g):
         """
         Forward pass for the dual network. The inputs are all current
-        duals and the first dual variable. The output is the updated dual.
+        duals, second dual variable, and the sinogram. The output is the
+        updated dual.
 
         Parameters
         ----------
         dual : torch.Tensor
+            Primal variable at the current iteration.
+        f2 : torch.Tensor
+            Forward projection of second dual variable at the current
+            iteration.
+        g : torch.Tensor
+            Observed sinogram.
+
+        Returns
+        -------
+        update : torch.Tensor
+            Update for dual variable.
+        """
+
+        # Concatenate dual, f2 and g
+        input = torch.cat((dual, f2, g), 1)
+
+        # Permute the input to match the input shape of the dual network
+        # input = input.permute(0, 3, 1, 2)
+
+        # Pass through the network
+        result = self.conv1(input)
+        result = self.act1(result)
+        result = self.conv2(result)
+        result = self.act2(result)
+        result = self.conv3(result)
+
+        # Permute the result to match the output shape of the dual network
+        # result = result.permute(0, 2, 3, 1)
+
+        # Add the result to dual and return the updated dual
+        return dual + result
+
+
+class PrimalNet(nn.Module):
+    """
+    Implementation of the primal network, using a 3-layer CNN.
+    """
+    def __init__(self, n_primal):
+        """
+        Initalisation function for the primal network. The architecture is
+        3-layer CNN with PReLU activations, the first two layers have 32
+        channels, and the last layer has n_primal channels.
+
+        Parameters
+        ----------
+        n_primal : int
+            The number of primal channels in "history"
+        """
+        super(PrimalNet, self).__init__()
+
+        self.n_primal = n_primal
+
+        self.conv1 = nn.Conv2d(in_channels=n_primal + 1, out_channels=32,
+                               kernel_size=(3, 3), padding=1)
+        self.act1 = nn.PReLU(num_parameters=32, init=0.0)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32,
+                               kernel_size=(3, 3), padding=1)
+        self.act2 = nn.PReLU(num_parameters=32, init=0.0)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=n_primal,
+                               kernel_size=(3, 3), padding=1)
+
+        # Intialise the weights and biases
+        self._init_weights()
+
+    def _init_weights(self):
+        """
+        Initialises the weights of the network using the Xavier initialisation
+        method.
+        """
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # Initialise the weights using the Xavier initialisation method
+                nn.init.xavier_uniform_(m.weight)
+
+                # Initialise the biases to zero
+                nn.init.zeros_(m.bias)
+
+    def forward(self, primal, adj_h1):
+        """
+        Forward pass for the primal network. The inputs are all current
+        primals and the first primal variable. The output is the updated primal.
+
+        Parameters
+        ----------
+        primal : torch.Tensor
             Dual variable at the current iteration.
         fp_f1 : torch.Tensor
             Forward projection of the first primal variable at the current
@@ -115,11 +153,14 @@ class DualNet(nn.Module):
         Returns
         -------
         result : torch.Tensor
-            Update for dual variable.
+            Update for primal variable.
         """
 
-        # Concatenate dual and f1
-        input = torch.cat((dual, fp_f1 * adj_h1), 0)
+        # Concatenate primal and f1
+        input = torch.cat((primal, adj_h1), 1)
+
+        # Permute the input to match the input shape of the primal network
+        # input = input.permute(0, 3, 1, 2)
 
         # Pass through the network
         result = self.conv1(input)
@@ -128,17 +169,18 @@ class DualNet(nn.Module):
         result = self.act2(result)
         result = self.conv3(result)
 
-        # Add the result to dual and return the updated dual
-        return result
+        # Permute the result to match the output shape of the primal network
+        # result = result.permute(0, 2, 3, 1)
+
+        # Add the result to primal and return the updated primal
+        return primal + result
 
 
 class PrimalDualNet(nn.Module):
 
-    def __init__(self, sinogram, input_dimension=362, n_detectors=513,
+    def __init__(self, input_dimension=362, n_detectors=513,
                  n_angles=1000, n_primal=5, n_dual=5, n_iterations=10):
-        super().__init__()
-
-        self.sinogram = sinogram
+        super(PrimalDualNet, self).__init__()
 
         # Create projection geometries
         self.vg = ts.volume(shape=(1, input_dimension, input_dimension))
@@ -153,37 +195,60 @@ class PrimalDualNet(nn.Module):
         self.dual_list = nn.ModuleList([DualNet(n_dual)
                                         for _ in range(n_iterations)])
 
+        # Create class attributes for other parameters
+        self.n_primal = n_primal
+        self.n_dual = n_dual
+
         self.n_iterations = n_iterations
 
-    def forward(self):
-        # Initialise the primal and dual variables
-        # TODO: Change the dimensions of the primal and dual variables to class
-        #       attributes
+        self.opnorm = 349530.28125
 
-        primal = torch.zeros((1, 1, 362, 362))
-        dual = torch.zeros((1, 1, 513, 1000))
+
+    def operator_norm(A, num_iter=10):
+        x = torch.randn(A.domain_shape)
+        for i in range(num_iter):
+            x = A.T(A(x))
+            x /= torch.norm(x) # L2 vector-norm
+        return (torch.norm(A.T(A(x))) / torch.norm(x)).item()
+
+    def forward(self, sinogram):
+        # Initialise the primal and dual variables
+
+        primal = torch.zeros((1, self.n_primal, 362, 362))
+        dual = torch.zeros((1, self.n_dual, 1000, 513))
+
+        # mu_water = 0.02
 
         # Feed forward 10 times
         for i in range(self.n_iterations):
+            # print(dual)
             # Pass through the primal network; first forward project the primal
-            fp_f = self.forward_projector(primal[..., 1:2])
+            # fp_f = torch.exp(-mu_water *
+            #                  self.forward_projector(primal[:, 1:2, ...].squeeze(1)))
+
+            fp_f = self.forward_projector(primal[:, 1:2, ...].squeeze(1)) / self.opnorm
 
             # TODO: Need to add exponential transform
-
-            dual_update = self.primal_list[i].forward(dual, fp_f,
-                                                      self.sinogram)
-            dual += dual_update
+            dual = self.dual_list[i].forward(dual, fp_f.unsqueeze(1),
+                                             sinogram.unsqueeze(1) / self.opnorm)
 
             # Pass through the dual network; backward project the product of
             # the first primal and the first dual
-            adj_h = self.forward_projector.T(primal[..., 0:1] *
-                                             dual[..., 0:1])
-            primal_update = self.dual_list[i].forward(primal, adj_h)
-            primal += primal_update
 
-        return primal[..., 0:1]
+            # # Forward project the first primal
+            # input = primal[:, 0:1, ...].squeeze(1)
+            # input = -mu_water * torch.exp(-mu_water * self.forward_projector(input))
 
+            # input *= dual[:, 0:1, ...].squeeze(1)
+            # adj_h = self.forward_projector.T(input)
 
-model = PrimalDualNet(0)
-for name, param in model.named_parameters():
-    print(name, param.size())
+            # adj_h = self.forward_projector.T(dual[:, 0:1, ...].squeeze(1))
+
+            adj_h = self.forward_projector.T(dual[:, 0:1, ...].squeeze(1)) / self.opnorm
+
+            # print(adj_h)
+
+            primal = self.primal_list[i].forward(primal,
+                                                 adj_h.unsqueeze(1))
+
+        return primal[:, 0:1, ...]
