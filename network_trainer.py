1e3ad8480e341c6d05ddb6180022844e1bce1075
Larry Wang <dw661@cam.ac.uk>
2024-03-03 15:15:51 +0000

    Added checkpoint feature in training

--- network_trainer.py
+++ network_trainer.py
@@ -6,6 +6,7 @@ import utils
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import tomosipo as ts
+from tqdm import tqdm
 
 # Set a global seed for reproducibility
 torch.manual_seed(1029)
@@ -13,7 +14,7 @@ torch.manual_seed(1029)
 # Define a function that trains the network
 def train_network(input_dimension=362, n_detectors=543,
                   n_angles=1000, n_primal=5, n_dual=5, n_iterations=10,
-                  epochs=100, learning_rate=0.001, beta=0.99):
+                  epochs=200, learning_rate=0.001, beta=0.99, resume=False):
     loss_function = nn.MSELoss()
 
     # Set a global seed for reproducibility
@@ -32,6 +33,7 @@ def train_network(input_dimension=362, n_detectors=543,
     vg = ts.volume(size=(1/input_dimension, 1, 1), shape=(1, input_dimension, input_dimension))
     pg = ts.parallel(angles=n_angles, shape=(1, n_detectors), 
                      size=(1/input_dimension, n_detectors/input_dimension))
+    
 
     model = PrimalDualNet(input_dimension=input_dimension,
                           vg=vg, pg=pg,
@@ -40,14 +42,19 @@ def train_network(input_dimension=362, n_detectors=543,
 
     optimizer = torch.optim.Adam(model.parameters(),
                                  lr=learning_rate, betas=(beta, 0.999))
-    # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     # Set up a scheduler to set up cosine annealing
-    # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
+    
+    if resume:
+        checkpoint = torch.load("checkpoint.pt")
+        model.load_state_dict(checkpoint["model_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
+        epoch = checkpoint["epoch"]
+        loss = checkpoint["loss"]
 
 
-    # TODO: Remove for later - Just training on one sample only
-    # to check if it works for now
     training_data = next(iter(train_dataloader))
 
     ground_truth = training_data[1].cuda()
@@ -57,7 +64,7 @@ def train_network(input_dimension=362, n_detectors=543,
     
     observation.cuda()
 
-    for i in range(epochs):
+    for epoch in range(epochs):
 
         output = model.forward(observation).squeeze(1)
         loss = loss_function(output, ground_truth)
@@ -75,15 +82,28 @@ def train_network(input_dimension=362, n_detectors=543,
         optimizer.step()
 
         # TODO: Add scheduler update later
-        # scheduler.step()
+        scheduler.step()
 
         # Print out the loss, weights and biases in the model
-        print(f"Epoch {i + 1}/{epochs}, Loss: {loss.item()}")
+        # print(f"Epoch {i + 1}/{epochs}, Loss: {loss.item()}")
+
+        if epoch % 10 == 0:
+           save_checkpoint(epoch, model, optimizer, scheduler, loss, f"checkpoint.pt")
+
 
     return model
 
+def save_checkpoint(epoch, model, optimizer, scheduler, loss, file):
+    torch.save( {
+        "epoch": epoch,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "scheduler_state_dict": scheduler.state_dict(),
+        "loss": loss}, file
+    )
 
-model = train_network(n_primal=5, n_dual=5)
+
+model = train_network(n_primal=5, n_dual=5, resume=True)
 # Predict the output
 
 def evaluate_model(model, input_path, target_path):
